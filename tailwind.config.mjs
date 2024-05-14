4667fd57240c02395824b745a3071cdf408989c2
Th1nkK1D <witheep@gmail.com>
2024-05-14 15:40:14 +0700

    feat: add typography

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,22 @@
 import daisyui from 'daisyui';
 import Config from './e-initiative.config.mjs';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	plugins: [daisyui],
 	theme: {
-		extend: {},
+		fontFamily: Object.entries(Config.theme.fonts).reduce(
+			(obj, [key, fontFamily]) => ({
+				...obj,
+				[key]: [
+					fontFamily.includes(' ') ? `"${fontFamily}"` : fontFamily,
+					...defaultTheme.fontFamily.sans,
+				],
+			}),
+			{},
+		),
 	},
 	daisyui: {
 		themes: [
