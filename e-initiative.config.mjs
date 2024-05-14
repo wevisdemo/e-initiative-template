625aba61a1390e865a37eb9122c4038b9d9bac2e
Th1nkK1D <witheep@gmail.com>
2024-05-14 15:40:14 +0700

    feat: add typography

--- e-initiative.config.mjs
+++ e-initiative.config.mjs
@@ -12,5 +12,14 @@ export default {
 			'base-300': '#E5E6E6',
 			info: '#F8EB54',
 		},
+		fonts: {
+			body: 'IBM Plex Sans Thai Looped',
+			'heading-fixed': 'IBM Plex Sans Thai',
+			'heading-responsive': 'IBM Plex Sans Thai Looped',
+			link: 'IBM Plex Sans Thai Looped',
+		},
+		stylesheets: [
+			'https://fonts.googleapis.com/css2?family=Noto+Sans+Thai+Looped:wght@100;200;300;400;500;600;700;800;900&family=Noto+Sans+Thai:wght@100..900&display=swap',
+		],
 	},
 };
