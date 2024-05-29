ea149e10be4a336117ab3c3d90255f9beab754d8
Th1nkK1D <witheep@gmail.com>
2024-05-29 16:19:37 +0700

    feat: extract offline and volunteer configuration

--- e-initiative.config.mjs
+++ e-initiative.config.mjs
@@ -8,6 +8,24 @@ export default {
 	pettition: {
 		endDate: new Date('2024-10-10 GMT+7'),
 		expectedSignatures: 10000,
+		offline: {
+			formUrl: '/#url-to-form',
+			formExampleImageUrl: 'https://placehold.co/280x397?text=example',
+			headquarter: {
+				name: 'ชื่อสถานที่',
+				address: 'ที่อยู่ บ้านเลขที่ จังหวัด ...',
+				embedGoogleMapUrl:
+					'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3873.298251120154!2d100.56221937605227!3d13.881106394284691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e28323d10290b5%3A0x946e22bc8754445a!2sOffice%20of%20Election%20Commission%20of%20Thailand!5e0!3m2!1sen!2sth!4v1716288187164!5m2!1sen!2sth',
+			},
+			volunteer: {
+				registrationFormUrl: '/#volunteer-register',
+				assetsUrl: '/#asset',
+				contact: {
+					name: 'ชื่อช่องทาง',
+					url: '/#contact',
+				},
+			},
+		},
 	},
 	theme: {
 		colors: {
--- src/components/HeadquarterMap.svelte
+++ src/components/HeadquarterMap.svelte
@@ -1,6 +1,10 @@
+<script>
+	import Config from '../../e-initiative.config.mjs';
+</script>
+
 <iframe
 	title="แผนที่"
-	src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3873.298251120154!2d100.56221937605227!3d13.881106394284691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e28323d10290b5%3A0x946e22bc8754445a!2sOffice%20of%20Election%20Commission%20of%20Thailand!5e0!3m2!1sen!2sth!4v1716288187164!5m2!1sen!2sth"
+	src={Config.pettition.offline.headquarter.embedGoogleMapUrl}
 	class="h-96 w-full"
 	loading="lazy"
 	referrerpolicy="no-referrer"
