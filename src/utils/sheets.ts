15b816b5143ef87e8a788180f96cf49f636bec9f
Th1nkK1D <witheep@gmail.com>
2024-06-19 16:47:46 +0700

    feat: add locations page

--- src/utils/sheets.ts
+++ src/utils/sheets.ts
@@ -1,8 +1,12 @@
 import { Spreadsheet } from 'sheethuahua';
 import Config from '../../e-initiative.config.mjs';
 import { offlineSignatureTable } from '../models/offline-signature';
+import { locationTable } from '../models/location';
 
-const sheets = Spreadsheet(Config.sheets?.id || '', [offlineSignatureTable]);
+const sheets = Spreadsheet(Config.sheets?.id || '', [
+	offlineSignatureTable,
+	locationTable,
+]);
 
 export async function countOfflineSignatures() {
 	return (await sheets.get('offline-signature')).reduce(
@@ -10,3 +14,22 @@ export async function countOfflineSignatures() {
 		0,
 	);
 }
+
+export async function getLocations() {
+	const locations = await sheets.get('locations');
+	const uniqueProvinces = [
+		...new Set(locations.map(({ province }) => province)),
+	].sort((a, z) => a.localeCompare(z));
+
+	return {
+		totalLocations: locations.length,
+		locationByProvinces: uniqueProvinces.map((province) => ({
+			province,
+			locations: locations.filter((location) => location.province === province),
+		})),
+	};
+}
+
+export type LocationByProvince = Awaited<
+	ReturnType<typeof getLocations>
+>['locationByProvinces'][number];
