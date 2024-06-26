69adbc2ae5096d51d44f1c894ffa3b6d4b17accb
Th1nkK1D <witheep@gmail.com>
2024-06-26 21:50:50 +0700

    fix: support deployment with non root path

--- e-initiative.config.mjs
+++ e-initiative.config.mjs
@@ -10,7 +10,7 @@ export default {
 		endDate: new Date('2024-10-10 GMT+7'),
 		expectedSignatures: 10000,
 		offline: {
-			formUrl: '/petition-form.pdf',
+			formUrl: `${import.meta.env.BASE_URL}/petition-form.pdf`,
 			formExampleImageUrl: 'https://placehold.co/280x397?text=example',
 			headquarter: {
 				name: 'ชื่อสถานที่',
@@ -19,11 +19,11 @@ export default {
 					'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3873.298251120154!2d100.56221937605227!3d13.881106394284691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e28323d10290b5%3A0x946e22bc8754445a!2sOffice%20of%20Election%20Commission%20of%20Thailand!5e0!3m2!1sen!2sth!4v1716288187164!5m2!1sen!2sth',
 			},
 			volunteer: {
-				registrationFormUrl: '/#volunteer-register',
-				assetsUrl: '/#asset',
+				registrationFormUrl: '#volunteer-register',
+				assetsUrl: '#asset',
 				contact: {
 					name: 'ชื่อช่องทาง',
-					url: '/#contact',
+					url: '#contact',
 				},
 			},
 		},
