356097fb9d2bd856abdad939483fccb6c3c268da
Withee Poositasai <witheep@gmail.com>
2025-04-09 16:33:10 +0700

    fix: update sheets id config

--- e-initiative.config.mjs
+++ e-initiative.config.mjs
@@ -52,7 +52,7 @@ export default {
 		],
 	},
 	sheets: {
-		id: '1PPl4bIKrNC68nc9sWDp7aolnTv0xiHZYQRlsvnRpAHc',
+		id: '1lV3AnEUM1bmTfOFbSn5ukvT2r3XmFrilOS4ewhJtc0Y',
 		enableOfflineSignature: false,
 		enableLocations: true,
 	},
