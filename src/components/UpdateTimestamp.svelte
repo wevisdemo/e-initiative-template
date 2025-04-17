8e3c061f03082009fd5245053665ff05cfe2a5ab
Th1nkK1D <witheep@gmail.com>
2025-04-17 22:11:29 +0200

    fix: use client timezone and improve format for last update timestamp

--- src/components/UpdateTimestamp.svelte
+++ src/components/UpdateTimestamp.svelte
@@ -1,6 +1,26 @@
-<p class="body-01 opacity-50 {$$restProps.class}">
-	อัปเดตข้อมูล {new Date().toLocaleString('TH-th', {
-		dateStyle: 'medium',
-		timeStyle: 'short',
-	})}
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let date: Date;
+
+	let updatedText = '';
+
+	onMount(() => {
+		const [day, month, year, time] = new Date(date)
+			.toLocaleString('TH-th', {
+				dateStyle: 'medium',
+				timeStyle: 'short',
+			})
+			.split(' ');
+
+		updatedText = `วันที่ ${day} ${month} ${year.slice(2, 4)} เวลา ${time} น.`;
+	});
+</script>
+
+<p
+	class="body-01 opacity-50 {updatedText
+		? 'block'
+		: 'invisible'} {$$restProps.class}"
+>
+	อัปเดตข้อมูลล่าสุด {updatedText}
 </p>
